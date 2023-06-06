915f56357eeaf4deb7b4c27dfb93b2c6c71d2735
Juan David <120610426+JUAN57755@users.noreply.github.com>
2023-06-06 16:09:10 -0400

    Update kmain.c

--- kmain.c
+++ kmain.c
@@ -1,6 +1,5 @@
 /* The C function */
-    void kmain()
+    int kmain(int arg1, int arg2, int arg3)
     {
-      
+        return arg1 + arg2 + arg3;
     }
-call kmain                 ; call the function
